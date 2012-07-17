a3deb4887e2e765ac195fea0d6fe3be2a2e51dc3
Novikov Andrey <envek@envek.name>
2012-07-17 16:13:36 +1000

    Make client to be configurable via command line arguments

--- main.py
+++ main.py
@@ -8,30 +8,42 @@ Author:     Sergey Lalov
 Date:       2011-03-30
 License:    GPL
 Target:     Cross Platform
-Require:    Python 2.6+. Modules: zope.interface, twisted
+Require:    Python 2.7+. Modules: zope.interface, twisted
 """
 from twisted.internet import reactor
 
 from http_mjpeg_client import MJPEGFactory
 
+import argparse
+
 def processImage(img):
     'This function is invoked by the MJPEG Client protocol'
     # Process image
     # Just save it as a file in this example
-    f = open('frame.jpg', 'wb')
+    f = open(config['filename'], 'wb')
     f.write(img)
     f.close()
     
+config = {
+    'callback': processImage,
+}
+
 def main():
-    print 'Python M-JPEG Over HTTP Client 0.1'
+    # Retrieve configuration from command line arguments
+    parser = argparse.ArgumentParser(description='Tool for grabbing frames from MJPEG over HTTP video stream to image file.')
+    parser.add_argument('--request', metavar='PATH', required=True,
+                        help='a path in the server to request for the videostream')
+    parser.add_argument('--ip', required=True, help='Remote server IP-address')
+    parser.add_argument('--port', type=int, default=80,
+                        help='Remote server port (default: 80)')
+    parser.add_argument('--login', help='Username for authentication (optional)')
+    parser.add_argument('--password', help='Password for authentication (optional)')
+    parser.add_argument('--filename', default='frame.jpg', help='A file name to save frames (default: frame.jpg)')
+    args = parser.parse_args()
     # Define connection parameters, login and password are optional.
-    config = {'request': '/?action=stream',
-              'login': 'admin',
-              'password': 'admin',
-              'ip': '127.0.0.1',
-              'port': 8080,
-              'callback': processImage}
+    config.update(vars(args))
     # Make a connection
+    print 'Python M-JPEG Over HTTP Client 0.1'
     reactor.connectTCP(config['ip'], config['port'], MJPEGFactory(config))
     reactor.run()    
     print 'Python M-JPEG Client stopped.'
